9a630b2f242fafbe4b4555e66c90140c2c96a280
Julien Ringenbach <Julien Ringenbach>
2017-02-01 21:37:17 +0100

    Création des constructeurs et des accesseurs de Map et Niveau

--- FearTheDungeon/Map.cs
+++ FearTheDungeon/Map.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace FearTheDungeon
 {
+    /// <summary>
+    /// Permet de créer une carte pour un niveau
+    /// </summary>
     class Map
     {
+
+        // *******************************
+        //          ATTRIBUTS
+        // *******************************
+
+        /// <summary>
+        /// Nombre de lignes composant la carte d'un niveau
+        /// </summary>
+        private int nombreLignes;
+
+        /// <summary>
+        /// Nombre de colonnes composant la carte d'un niveau
+        /// </summary>
+        private int nombreColonnes;
+
+        // *******************************
+        //          CONSTRUCTEURS
+        // *******************************
+
+        public Map()
+        {
+
+        }
+
+        /// <summary>
+        /// Permet de créer la carte en indiquant le nombre de lignes et de colonnes qu'elle possèdera
+        /// </summary>
+        /// <param name="lignes"></param>
+        /// <param name="colonnes"></param>
+        public Map(int lignes, int colonnes)
+        {
+
+        }
+
+        // ***************************************
+        //          ACCESSEURS ET MUTATEURS
+        // ***************************************
     }
 }
--- FearTheDungeon/Niveau.cs
+++ FearTheDungeon/Niveau.cs
@@ -8,5 +8,43 @@ namespace FearTheDungeon
 {
     class Niveau
     {
+
+        // *******************************
+        //          ATTRIBUTS
+        // *******************************
+
+        /// <summary>
+        /// Contient les données relatives à la carte du niveau
+        /// </summary>
+        Map carteDuNiveau = new Map();
+
+        // *******************************
+        //          CONSTRUCTEURS
+        // *******************************
+
+        /// <summary>
+        /// CONSTRUCTEURS
+        /// </summary>
+        public Niveau()
+        {
+
+        }
+
+        // ***************************************
+        //          ACCESSEURS ET MUTATEURS
+        // ***************************************
+
+        public Map CarteDuNiveau
+        {
+            get
+            {
+                return carteDuNiveau;
+            }
+
+            set
+            {
+                carteDuNiveau = value;
+            }
+        }
     }
 }
