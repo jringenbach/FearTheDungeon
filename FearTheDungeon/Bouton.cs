3c8f598ca574bf6f8f130fde2820931d87df6052
jringenbach <34011-14-08@exchange.ad.afpanet>
2017-02-09 15:57:07 +0100

    Ajout de Bouton.cs et Porte.cs

---
+++ FearTheDungeon/Bouton.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FearTheDungeon
+{
+	/// <summary>
+	/// Il peut être activé par la présence du joueur ou d'un bloc. Il déclenchera alors une action sur la map
+	/// </summary>
+	class Bouton : MapElement
+	{
+		/// <summary>
+		/// Porte que le bouton va ouvrir quand il sera déclenché
+		/// </summary>
+		private Porte _porteQueJouvre;
+
+		// *******************************
+		//          CONSTRUCTEURS
+		// *******************************
+
+		/// <summary>
+		/// CONSTRUCTEUR SANS PARAMETRE DE BOUTON
+		/// </summary>
+		public Bouton() : base()
+		{
+			Symbole = 'b';
+		}
+
+		/// <summary>
+		/// CONSTRUCTEUR DE LA CLASSE BOUTON
+		/// </summary>
+		/// <param name="positionElement"></param>
+		public Bouton (int[] positionElement) : base(positionElement)
+		{
+			this.PositionElement = positionElement;
+			this.Symbole = 'b';
+		}
+
+		/// <summary>
+		/// CONSTRUCTEUR DE LA CLASSE BOUTON
+		/// </summary>
+		/// <param name="positionX">Numéro de la ligne</param>
+		/// <param name="positionY">Numéro de la colonne</param>
+		public Bouton (int positionX, int positionY) : base()
+		{
+			PositionElement[0] = positionX;
+			PositionElement[1] = positionY;
+			this.Symbole = 'b';
+		}
+
+		/// <summary>
+		/// CONSTRUCTEUR DE LA CLASSE BOUTON
+		/// </summary>
+		/// <param name="positionX">Numéro de la ligne</param>
+		/// <param name="positionY">Numéro de la colonne</param>
+		public Bouton(int positionX, int positionY, Porte porteQueJouvre) : base()
+		{
+			PositionElement[0] = positionX;
+			PositionElement[1] = positionY;
+			PorteQueJouvre = porteQueJouvre;
+			this.Symbole = 'b';
+		}
+
+		public Porte PorteQueJouvre
+		{
+			get
+			{
+				return _porteQueJouvre;
+			}
+
+			set
+			{
+				_porteQueJouvre = value;
+			}
+		}
+	}
+}
---
+++ FearTheDungeon/Porte.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FearTheDungeon
+{
+	/// <summary>
+	/// Bloque le passage au joueur, mais peut être ouverte grâce à un déclencheur particulier
+	/// </summary>
+	class Porte : MapElement
+	{
+
+		// *******************************
+		//          ATTRIBUTS
+		// *******************************
+
+		private bool _ouverte;
+		private bool _ouverteInitiale;
+		private char _symboleInitial;
+
+		// *******************************
+		//          CONSTRUCTEURS
+		// *******************************
+
+		/// <summary>
+		/// CONSTRUCTEUR SANS PARAMETRE DE LA CLASSE PORTE
+		/// </summary>
+		public Porte() : base()
+		{
+			Symbole = 'P';
+			SymboleInitial = 'P';
+			Nom = "Porte";
+		}
+
+		/// <summary>
+		/// CONSTRUCTEUR DE LA CLASSE PORTE
+		/// </summary>
+		/// <param name="positionElement">La case 0 contient le numéro de ligne et la case 1 contient le numéro de colonne</param>
+		public Porte(int[] positionElement) : base(positionElement)
+		{
+			this.PositionElement = positionElement;
+			this.Symbole = 'P';
+			SymboleInitial = 'P';
+			Nom = "Porte";
+		}
+
+		/// <summary>
+		/// CONSTRUCTEUR DE LA CLASSE PORTE
+		/// </summary>
+		/// <param name="positionX">Numéro de la ligne</param>
+		/// <param name="positionY">Numéro de la colonne</param>
+		public Porte(int positionX, int positionY) : base()
+		{
+			PositionElement[0] = positionX;
+			PositionElement[1] = positionY;
+			this.Symbole = 'P';
+			SymboleInitial = 'P';
+			Nom = "Porte";
+		}
+
+		/// <summary>
+		/// CONSTRUCTEUR DE LA CLASSE PORTE
+		/// </summary>
+		/// <param name="positionX">Numéro de la ligne</param>
+		/// <param name="positionY">Numéro de la colonne</param>
+		public Porte(int positionX, int positionY, bool ouverte) : base()
+		{
+			PositionElement[0] = positionX;
+			PositionElement[1] = positionY;
+			Ouverte = ouverte;
+			this.Symbole = 'P';
+			SymboleInitial = 'P';
+			Nom = "Porte";
+			OuverteInitiale = ouverte;
+	}
+
+		// ***************************************
+		//          ACCESSEURS ET MUTATEURS
+		// ***************************************
+
+		/// <summary>
+		/// Vaut true si la porte est ouverte, false sinon.
+		/// </summary>
+		public bool Ouverte
+		{
+			get
+			{
+				return _ouverte;
+			}
+
+			set
+			{
+				_ouverte = value;
+			}
+		}
+
+		/// <summary>
+		/// Garde en mémoire si la porte était ouverte initialement ou non
+		/// </summary>
+		public bool OuverteInitiale
+		{
+			get
+			{
+				return _ouverteInitiale;
+			}
+
+			set
+			{
+				_ouverteInitiale = value;
+			}
+		}
+
+		/// <summary>
+		/// Garde en mémoire le symbole Initial au symbole de la classe Porte
+		/// </summary>
+		public char SymboleInitial
+		{
+			get
+			{
+				return _symboleInitial;
+			}
+
+			set
+			{
+				_symboleInitial = value;
+			}
+		}
+
+		// ***************************************
+		//          METHODE
+		// ***************************************
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public void EtatInitial()
+		{
+			this.Symbole = SymboleInitial;
+			this.Ouverte = OuverteInitiale;
+		}
+	}
+}
