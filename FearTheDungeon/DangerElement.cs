23f808aff00ce208215d8806415d2572931258f8
jringenbach <34011-14-08@exchange.ad.afpanet>
2017-02-10 10:41:05 +0100

    Implémentation de la classe DangerElement.cs

---
+++ FearTheDungeon/DangerElement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FearTheDungeon
+{
+	/// <summary>
+	/// Contient la position des cases
+	/// </summary>
+	class DangerElement : MapElement
+	{
+	}
+}
